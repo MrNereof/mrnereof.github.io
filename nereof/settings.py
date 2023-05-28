05d229505a37f2e635e5fad926279fbe13cd08c8
MrNereof <mrnereof@gmail.com>
2023-05-28 17:51:53 +0300

    Fix allowed-hosts

--- nereof/settings.py
+++ nereof/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-)wy0h9z6x4o3t4@=s_keq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'true') == 'true'
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*').split()]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split()
 
 
 # Application definition

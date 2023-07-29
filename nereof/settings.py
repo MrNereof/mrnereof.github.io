49348c644ea63a91a9e5b1d7bf88e405de567d99
MrNereof <mrnereof@gmail.com>
2023-07-29 15:12:25 +0300

    Add .env files support

--- nereof/settings.py
+++ nereof/settings.py
@@ -17,7 +17,9 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / ".env")
+ENV_DIR = BASE_DIR / ".env"
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production

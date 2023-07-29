8f24ef602b4cf19915d6c78080f8f5bc6862b054
MrNereof <mrnereof@gmail.com>
2023-07-29 15:17:25 +0300

    Add .env files support

--- manage.py
+++ manage.py
@@ -2,13 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
-from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
-    load_dotenv(Path(__file__).resolve().parent / ".env")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nereof.settings')
     try:
         from django.core.management import execute_from_command_line
